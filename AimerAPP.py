2c42236b094202b080ead9982600fbe0398542e1
DonerKebab8989 <24e1808j@shinshu-u.ac.jp>
2025-11-13 18:15:54 +0900

    2

--- AimerAPP.py
+++ AimerAPP.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.title("ホーム")
 st.header("わはははは")
 
+home = st.Page("AimerAPP.py", title="ホーム")
+survey = st.Page("survey.py", title="診断テスト")
+result = st.Page("result.py", title="結果")
+fav = st.Page("fav.py", title="オススメ")
+
+nav = st.navigation([home, survey, result, fav])
+nav.run()
