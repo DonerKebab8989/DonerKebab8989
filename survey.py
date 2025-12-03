fcecdf26bbbf4615912fa7450f7967faabcf71d5
DonerKebab8989 <24e1808j@shinshu-u.ac.jp>
2025-12-04 02:57:10 +0900

    1

--- survey.py
+++ survey.py
@@ -10,17 +10,41 @@ st.title("診断テスト")
 st.subheader("以下の質問に直感でお答えください。")
 q1 = ["明るい曲", "暗い曲", "どちらともいえない"]
 selection1 = st.segmented_control(
-    "🎤歌詞の意味は関係なく、聞いた感じの雰囲気はどっちが好き？", q1, selection_mode="single"
+    "🎤歌詞の意味は関係なく、聞いた感じの雰囲気はどっちが好き？", q1, selection_mode="single", key="q1"
+)
+st.markdown("""
+<style>
+div[data-testid="stSegmentedControl"][aria-label="q1"] button {
+    color: black !important;
+}
+</style>
+""", unsafe_allow_html=True
 )
 st.session_state["answer1"] = selection1
 q2 = ["恋愛", "友情", "学校", "青春", "応援", "家族", "季節・イベント・自然", "別れ", "アニメ", "ドラマ・映画"]
 selection2 = st.segmented_control(
-    "🎤好きな音楽のジャンルは？複数選択できます♪", q2, selection_mode="multi"
+    "🎤好きな音楽のジャンルは？複数選択できます♪", q2, selection_mode="multi", key="q2"
+)
+st.markdown("""
+<style>
+div[data-testid="stSegmentedControl"][aria-label="q2"] button {
+    color: black !important;
+}
+</style>
+""", unsafe_allow_html=True
 )
 st.session_state["answer2"] = selection2
 q3 = ["アップテンポ", "ゆっくり", "リズミカル", "あてはまるものはない"]
 selection3 = st.segmented_control(
-    "🎤どんなテンポが好き？", q3, selection_mode="single"
+    "🎤どんなテンポが好き？", q3, selection_mode="single", key="q3"
+)
+st.markdown("""
+<style>
+div[data-testid="stSegmentedControl"][aria-label="q3"] button {
+    color: black !important;
+}
+</style>
+""", unsafe_allow_html=True
 )
 st.session_state["answer3"] = selection3
 st.page_link("result.py", label="→診断結果はこちら")
